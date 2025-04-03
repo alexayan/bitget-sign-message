c14907ef7ae85675121453a69e0fd615ea46989d
yan <yanhaibiao1991@gmail.com>
2025-04-03 19:04:31 +0800

    init

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,28 @@
 import "@/styles/globals.css";
+import '@mysten/dapp-kit/dist/index.css';
 import type { AppProps } from "next/app";
+import {
+  createNetworkConfig,
+  SuiClientProvider,
+  WalletProvider,
+} from "@mysten/dapp-kit";
+import { getFullnodeUrl } from "@mysten/sui/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+// Config options for the networks you want to connect to
+const { networkConfig } = createNetworkConfig({
+  mainnet: { url: getFullnodeUrl("mainnet") },
+});
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <SuiClientProvider networks={networkConfig} defaultNetwork="mainnet">
+        <WalletProvider autoConnect>
+          <Component {...pageProps} />
+        </WalletProvider>
+      </SuiClientProvider>
+    </QueryClientProvider>
+  );
 }
